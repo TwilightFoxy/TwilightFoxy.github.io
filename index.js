7031b870b0bab40cc49df1ae4f41cb68fbba8512
TwilightFoxy <62305710+TwilightFoxy@users.noreply.github.com>
2022-06-12 12:57:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,16 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
 	tg.sendData("some string that we need to send");
 	//при клике на основную кнопку отправляем данные в строковом виде
 });
+
+let usercard = document.getElementById("usercard"); //получаем блок usercard 
+
+let profName = document.createElement('p'); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+//выдем имя, "фамилию", через тире username и код языка
+usercard.appendChild(profName); //добавляем 
+
+let userid = document.createElement('p'); //создаем еще параграф 
+userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
+usercard.appendChild(userid); //добавляем
